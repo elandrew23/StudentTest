6dca61250e3b731c4b0f1c737b281905b747ba16
elandrew23 <elandrew@sheridancollege.ca>
2024-09-12 18:13:18 -0400

    First publication.
    Note: This is just an in-class activity/review on how to publish to
    GitHub via NetBeans.

---
+++ src/StudentDetailsPrint.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Salanewt
+ */
+public class StudentDetailsPrint {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Andrew");
+        s1.setSid(1);
+        Student s2 = new Student();
+        s2.setName("John");
+        s2.setSid(2);
+        Student s3 = new Student();
+        s3.setName("Syd");
+        s3.setSid(3);
+        //Classname[] list = new Classname[##];
+        Student[] list = new Student[3];  // array of objects
+        list[0] = s1;
+        list[1] = s2;
+        list[2] = s3;
+        // Objects stored in array\
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i]);
+            System.out.println(list[i].getSid() + ": " + list[i].getName());
+        }
+        
+
+    }
+}
